0489f97e6542f329e13f2c8f6ca3902e744b57dd
galej <jack.gale@tabcorp.com.au>
2026-04-12 11:24:53 +1000

    Requirement 3: Tests Complete -- adding test files

---
+++ ToyRobotTest.Tests/SimulatorTests.cs
@@ -0,0 +1,59 @@
+﻿namespace ToyRobotTest.Tests;
+
+public class SimulatorTests
+{
+    [Fact]
+    public void SimulatorExample1Test()
+    {
+        //  Arrange
+        // Instantiate objects and set expectedReport
+        Robot robot = new Robot();
+        Simulator simulator = new Simulator(robot);
+
+        string filePath = "TestFiles/Example1.txt";
+        string expectedReport = "0,1,NORTH";
+
+        // Act
+        // Read file line by line
+        ReadFile(filePath, simulator);
+
+        // Set actual report string
+        string actualReport = robot.Report();
+
+        // Assert
+        Assert.Equal(expectedReport, actualReport);
+    }
+
+    [Fact]
+    public void SimulatorExample2Test()
+    {
+        //  Arrange
+        // Instantiate objects, set file path and expectedReport string
+        Robot robot = new Robot();
+        Simulator simulator = new Simulator(robot);
+
+        string filePath = "TestFiles/Example2.txt";
+        string expectedReport = "0,0,WEST";
+
+        // Act
+        // Read file line by line
+        ReadFile(filePath, simulator);
+
+        // Set actual report string
+        string actualReport = robot.Report();
+
+        // Assert
+        Assert.Equal(expectedReport, actualReport);
+    }
+
+    public void ReadFile(String filePath, Simulator simulator)
+    {
+        foreach (string line in File.ReadLines(filePath))
+        {
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                simulator.Execute(line);
+            }
+        }
+    }
+}
